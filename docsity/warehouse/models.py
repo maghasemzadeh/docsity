840082ff8791c0a6e944cb479e20fc89aa1805ca
Mohammad Amin <m.agasemzadeh@gmail.com>
2022-07-29 23:25:24 +0430

    add str functions

--- docsity/warehouse/models.py
+++ docsity/warehouse/models.py
@@ -7,18 +7,32 @@ class University(TimeStampedModel):
     logo = models.ImageField()
     city = models.CharField(max_length=127)
 
+    def __str__(self):
+        return self.name
+
 
 class Faculty(TimeStampedModel):
     name = models.CharField(max_length=127)
 
+    def __str__(self):
+        return self.name
+
 
 class Course(TimeStampedModel):
     title = models.CharField(max_length=127)
+
     # faculty = models.ForeignKey(on_delete=models.SET_NULL)
+
+    def __str__(self):
+        return self.title
 
 
 class EducationalYear(TimeStampedModel):
     year = models.IntegerField()
+
+    def __str__(self):
+        next_year = self.year + 1
+        return f"{self.year}-{next_year}"
 
 
 class Professor(TimeStampedModel):
@@ -26,6 +40,9 @@ class Professor(TimeStampedModel):
     family_name = models.CharField(max_length=127)
     title = models.CharField(max_length=63)
     birthday_year = models.IntegerField(null=True, blank=True)
+
+    def __str__(self):
+        return f"{self.title} {self.name} {self.family_name}"
 
 
 class Content(TimeStampedModel):
@@ -35,5 +52,8 @@ class Content(TimeStampedModel):
     year = models.ForeignKey(to=EducationalYear, on_delete=models.SET_NULL)
 
     file = models.FileField()
+
+    def __str__(self):
+        return self.file.name
 
 # Create your models here.
