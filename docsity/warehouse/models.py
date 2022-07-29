8007c123db10f43e3c5a5c0963ab1dfe8f4c1791
Mohammad Amin <m.agasemzadeh@gmail.com>
2022-07-29 20:59:20 +0430

    add initial models

--- docsity/warehouse/models.py
+++ docsity/warehouse/models.py
@@ -1,3 +1,33 @@
 from django.db import models
+from model_utils.models import TimeStampedModel
+
+
+class University(TimeStampedModel):
+    name = models.CharField(max_length=511)
+    logo = models.ImageField()
+    city = models.CharField(max_length=127)
+
+
+class Faculty(TimeStampedModel):
+    name = models.CharField(max_length=127)
+
+
+class Course(TimeStampedModel):
+    title = models.CharField(max_length=127)
+    # faculty = models.ForeignKey(on_delete=models.SET_NULL)
+
+
+class EducationalYear(TimeStampedModel):
+    year = models.IntegerField()
+
+
+class Professor(TimeStampedModel):
+    name = models.CharField(max_length=127)
+    family_name = models.CharField(max_length=127)
+    title = models.CharField(max_length=63)
+    birthday_year = models.IntegerField(null=True, blank=True)
+
+
+
 
 # Create your models here.
