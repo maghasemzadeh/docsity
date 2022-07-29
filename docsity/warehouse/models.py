46cee1c40b17cd8eeb95edad58f29df6d14282fd
AhmadZafarani <ahmadzafarani2013@gmail.com>
2022-07-29 23:43:07 +0430

    delete year from content

--- docsity/warehouse/models.py
+++ docsity/warehouse/models.py
@@ -34,7 +34,6 @@ class Content(TimeStampedModel):
     university = models.ForeignKey(to=University, on_delete=models.SET_NULL)
     faculty = models.ForeignKey(to=Faculty, on_delete=models.SET_NULL)
     course = models.ForeignKey(to=Course, on_delete=models.SET_NULL)
-    year = models.ForeignKey(to=EducationalYear, on_delete=models.SET_NULL)
 
     file = models.FileField()
     type = models.PositiveSmallIntegerField(choices=ContentType.choices)
